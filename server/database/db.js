72b6231cd523f8868b56f278bcbf602bc69bd885
Amit5254 <amit@iiitmanipur.ac.in>
2022-12-16 11:32:21 +0530

    first-commit

---
+++ server/database/db.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+const Connection = () => {
+
+    const MONGODB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-ktiokxv-shard-00-00.h1flikz.mongodb.net:27017,ac-ktiokxv-shard-00-01.h1flikz.mongodb.net:27017,ac-ktiokxv-shard-00-02.h1flikz.mongodb.net:27017/?ssl=true&replicaSet=atlas-q6slm3-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+    mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+    mongoose.connection.on('connected', () => {
+        console.log('Database connected Successfully');
+    })
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Database disconnected');
+    })
+
+    mongoose.connection.on('error', () => {
+        console.log('Error while connecting with the database ', error.message);
+    })
+}
+
+export default Connection;
